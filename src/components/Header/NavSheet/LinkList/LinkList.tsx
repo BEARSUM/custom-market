f69138a59d602dfbc8090d2b4d239545a3475e12
SUMINMOON <sum95771@gmail.com>
2024-07-08 23:44:33 +0900

    feat: NavSheet link 클릭시 닫힘 기능

--- src/components/Header/NavSheet/LinkList/LinkList.tsx
+++ src/components/Header/NavSheet/LinkList/LinkList.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { NavLink } from 'react-router-dom';
+import { SheetClose } from '@/components/ui/sheet';
 
 /**
  * NavSheet에 쓰이는 LinkList 컴포넌트
@@ -14,14 +15,16 @@ const LinkList = (props: LinkListProps) => {
 
   return (
     <li>
-      <NavLink
-        to={path}
-        className={({ isActive }) => {
-          return isActive ? 'text-primary' : 'text-foreground';
-        }}
-      >
-        {children}
-      </NavLink>
+      <SheetClose asChild>
+        <NavLink
+          to={path}
+          className={({ isActive }) => {
+            return isActive ? 'text-primary' : 'text-foreground';
+          }}
+        >
+          {children}
+        </NavLink>
+      </SheetClose>
     </li>
   );
 };
