efb31b6c16a030388ebc51e26b945be12d82fe7f
SUMINMOON <sum95771@gmail.com>
2024-07-13 22:36:30 +0900

    feat: React Quill 커스텀 컴포넌트

---
+++ src/components/QuillEditor/QuillEditor.tsx
@@ -0,0 +1,35 @@
+import { forwardRef } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import ReactQuill, { ReactQuillProps } from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+/**
+ * ReactHookForm 과 같이 사용하기 위한 ReactQuill 컴포넌트
+ *
+ * @example
+ * <QuillEditor ref={quillRef} modules={modules} placeholder="내용을 입력해주세요." />
+ */
+
+const QuillEditor = forwardRef<ReactQuill, ReactQuillProps>(({ modules, placeholder }, ref) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name="content"
+      control={control}
+      render={({ field: { value, onChange } }) => (
+        <ReactQuill
+          placeholder={placeholder}
+          modules={modules}
+          theme="snow"
+          className="h-56"
+          ref={ref}
+          value={value}
+          onChange={onChange}
+        />
+      )}
+    />
+  );
+});
+
+export default QuillEditor;
